7b34554a164f910eebf3e01170b71fdeef95f4b2
Maxmiliano F Braga <maxmiliano@gmail.com>
2022-09-23 10:38:51 -0300

    Add basic model specs and email validator

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+
+  it { is_expected.to have_many(:orders) }
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to belong_to(:merchant) }
+  it { is_expected.to belong_to(:shopper) }
+
+  it { is_expected.to validate_presence_of(:merchant) }
+  it { is_expected.to validate_presence_of(:shopper) }
+
 end
